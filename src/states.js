5a003c1bf1a1c3ed7bc3896d1e5e5cb90efd2ec2
techbywes <dhspotless@gmail.com>
2023-05-23 23:01:30 +0100

    updated the  todo list

--- src/states.js
+++ src/states.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import deleteIcon from "./images/delete1.png";
+import penIcon from "./images/pen.png";
 
 const States = () => {
   const [ToDoList, setToDoList] = useState([]);
@@ -54,46 +55,55 @@ const States = () => {
 
   return (
     <>
-    <h1 className="app_titile">TODO APP</h1>
-    
-    <div className="App2">
-      <div className="taskCount">
-        <p>
-          You have {pendingTasksCount}{" "}
-          {pendingTasksCount === 1 ? "pending task" : "pending tasks"}
-        </p>
-      </div>
-      <div className="addTask">
-        <input
-          placeholder="Enter Your Todo Here"
-          id="input_btn"
-          value={newTask}
-          onChange={handleChange}
-          onKeyPress={handleKeyPress}
-        />
-        <button className="btn" onClick={addTask}>
-          +
-        </button>
-        <button className="reset-btn" onClick={resetList}>
-          Clear
-        </button>
-      </div>
+      <h1 className="app_titile">TODO APP
+      <img src={penIcon} className="pen_icon" /></h1>
 
-      <div className="list">
-        {ToDoList.map(task => (
-          <div
-            className={`task_delete_btn ${task.completed ? "completed" : ""}`}
-            key={task.id}
-          >
-            <p className="task_text">{task.taskName}</p>
+      <div className="App2">
+        <div className="taskCount">
+          <p>
+            You have {pendingTasksCount}{" "}
+            {pendingTasksCount === 1 ? "pending task" : "pending tasks"}
+          </p>
+        </div>
+        <div className="addTask">
+          <input
+            placeholder="Enter Your Todo Here"
+            id="input_btn"
+            value={newTask}
+            onChange={handleChange}
+            onKeyPress={handleKeyPress}
+          />
+          <button className="btn" onClick={addTask}>
+            +
+          </button>
+          <button className="reset-btn" onClick={resetList}>
+            Clear
+          </button>
+        </div>
 
-            <button className="delete_btn" onClick={() => deleteTask(task.id)}>
-              <img src={deleteIcon} className="delete_icon" />
-            </button>
-          </div>
-        ))}
+        <div className="list">
+          {ToDoList.map(task => (
+            <div
+              className={`task_delete_btn ${task.completed ? "completed" : ""}`}
+              key={task.id}
+            >
+
+              <div className="task_text_div">
+              <p className="task_text">{task.taskName}</p>
+              </div>
+
+              <div className="del_btb_div">
+              <button
+                className="delete_btn"
+                onClick={() => deleteTask(task.id)}
+              >
+                <img src={deleteIcon} className="delete_icon" />
+              </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </>
   );
 };
